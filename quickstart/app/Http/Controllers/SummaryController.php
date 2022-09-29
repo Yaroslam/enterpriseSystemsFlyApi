332e216b690208073fa4ecad7a3c1d69646d7061
Yaroslam <Yaroslam322@gmail.com>
2022-09-29 17:33:26 +0300

    add validation

--- quickstart/app/Http/Controllers/SummaryController.php
+++ quickstart/app/Http/Controllers/SummaryController.php
@@ -5,10 +5,17 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\Summary;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class SummaryController extends Controller
 {
     public function loadSummaryFromFile(Request $request){
+
+        $validator = Validator::make($request->all(), [ 'file' => 'mimes:csv']);
+        if ($validator->fails()) {
+            return  Response($validator->errors(), 400);
+        }
+
         $request->file('file')->store('files');
         $csvFile = file($request->file('file'));
         echo "1";
@@ -18,10 +25,10 @@ class SummaryController extends Controller
                 continue;
             }
             $data = str_getcsv($line);
-            var_dump($data);
-//            Summary::loadFromFile($data[0], $data[1], $data[2], $data[3], $data[4],  $data[5],
-//                $data[6], $data[7], $data[8]);
+            Summary::loadFromFile($data[0], $data[1], $data[2], $data[3], $data[4],  $data[5],
+                $data[6], $data[7], $data[8]);
         }
+        return Response([],200);
     }
 // TODO
 //  4)запись ответов опрошенных
