94445ca9aa24eaa1abe09ac850ef6d384d965a42
Yaroslam <Yaroslam322@gmail.com>
2022-10-08 00:45:37 +0300

    add cors

--- quickstart/config/cors.php
+++ quickstart/config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', "/*"],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', "/*", "/getSchedule"],
 
     'allowed_methods' => ['*'],
 
