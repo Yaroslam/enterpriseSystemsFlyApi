84acc4563efd7675bc0d601ea4e7a8f5314c76c4
Yaroslam <Yaroslam322@gmail.com>
2022-10-08 01:35:41 +0300

    add cors

--- quickstart/routes/web.php
+++ quickstart/routes/web.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-
+*/
 header('Access-Control-Allow-Origin:  *');
 header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
