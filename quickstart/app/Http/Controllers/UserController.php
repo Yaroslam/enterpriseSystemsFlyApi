0152e248054fb781086d2c5335c1c7e22218a6ca
Yaroslam <Vestnik700@gmail.com>
2022-11-12 15:48:52 +0300

    session fix

--- quickstart/app/Http/Controllers/UserController.php
+++ quickstart/app/Http/Controllers/UserController.php
@@ -63,7 +63,7 @@ class UserController extends Controller
             $response['numberOfCrashes'] = countCrashes($sessions);
             $response['curSessionTime'] = $sessionTime;
             $response['sessions'] = $sessions;
-            session(['email' => $user->Email, 'sessionStart' => $todayDate, 'spendTime' => $sessionTime])->save();
+            session(['email' => $user->Email, 'sessionStart' => $todayDate, 'spendTime' => $sessionTime]);
             inSystem::addUser($user->ID, date("Y-m-d H:i:s", $todayDate));
         }
         return Response($response, $codeStatus);
